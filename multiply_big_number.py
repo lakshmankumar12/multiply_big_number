4d2213791ac0dc3a18db13dd3637bdfa84402e8c
lnara002 <lakshman_kumar.narayanan@alcatel-lucent.com>
2016-01-14 09:49:52 -0800

    removed debug prints and made inputs as cmd-line args

--- multiply_big_number.py
+++ multiply_big_number.py
@@ -12,18 +12,14 @@ def init_char_to_num(char_to_num):
 def direct_multiply(num1,l1,num2,l2):
   a=int(num1)
   b=int(num2)
-  print("direct multiply for %s,%d and %s,%d"%(num1,l1,num2,l2))
   res=str(a*b)
   l = len(res)
   if l < l1+l2:
     res = "0" + res
-  print("returning %s"%res)
   return res
 
 def sum_big_num(num1, len_num1, num2, len_num2, carry_in):
   global char_to_num
-
-  print("sum for %s,%d and %s,%d carry:%d"%(num1,len_num1,num2,len_num2,carry_in))
 
   #pad both numbers to make them equi-length
   max_len = max(len_num1,len_num2)
@@ -39,14 +35,10 @@ def sum_big_num(num1, len_num1, num2, len_num2, carry_in):
     s = str(a%10) + s
     carry = a/10
 
-  print("returning %s,%d"%(s,carry))
   return (s,carry)
 
 
 def multiply_big_number(num1, len_num1, num2, len_num2):
-
-  input_str="muliply for %s,%d and %s,%d"%(num1,len_num1,num2,len_num2)
-  print(input_str)
 
   #pad both numbers to make them equi-length
   max_len = max(len_num1,len_num2)
@@ -79,43 +71,35 @@ def multiply_big_number(num1, len_num1, num2, len_num2):
   (adbc,adbc_carry) = sum_big_num(ad,lsb_size+msb_size,bc,lsb_size+msb_size,0)
   #result has lsb_size of cd as-is.
   result = bd[lsb_size:]
-  print("%s got ad:%s, bc:%s, adbc:%s adbc_carry=%d, result-so-far:%s"%(input_str,ad,bc,adbc, adbc_carry, result))
 
   #sum msb of bd with lsb of (ad+bc)
   (second_part_sum,second_part_carry) = sum_big_num(bd[:lsb_size],lsb_size,adbc[msb_size:],lsb_size,0)
   result = second_part_sum + result
-  print("%s got bd:%s, second_part_sum:%s, second_part_carry:%d, result-so-far:%s"%(input_str,bd,second_part_sum,second_part_carry, result))
 
   #sum lsb of ac with msb of (ad+bc)
   (third_part_sum,third_part_carry) = sum_big_num(ac[msb_size:],msb_size,adbc[:lsb_size],msb_size,second_part_carry)
   result = third_part_sum + result
-  print("%s got ac:%s, third_part_sum:%s, third_part_carry:%d, result-so-far:%s"%(input_str,ac,third_part_sum,third_part_carry,result))
 
   (forth_part_sum,discard) = sum_big_num(ac[:msb_size],msb_size,"",0,third_part_carry)
   result = forth_part_sum + result
 
   while len(result) > len_num1 + len_num2:
     if result[0] != "0":
-      print("current processing %s , len-result:%d, result:%s"%(input_str,len(result),result))
       sys.exit("issue");
     result = result[1:]
 
-  print("%s returns %s"%(input_str,result))
   return result
 
 if __name__ == '__main__':
   init_char_to_num(char_to_num)
-  comment='''
-  a="24234234"
-  b="22322"
-  r = multiply_big_number(a,len(a),b,len(b))
-  print (r)
-  '''
+
+  b = int(sys.argv[1])
+  n = int(sys.argv[2])
 
   powers = {}
   max_powers=8
-  powers[1] = "10"
-  for i in range(2,max_powers+1):
+  powers[0] = str(b)
+  for i in range(1,max_powers):
     s = powers[i-1]
     l = len(s)
     p = multiply_big_number(s,l,s,l)
@@ -123,17 +107,18 @@ if __name__ == '__main__':
       p = p[1:]
     powers[i] = p
   print("printing powers")
-  for i in range(1,max_powers+1):
-    print("power of %d is %s"%(i,powers[i]))
-  n = 100
+  for i in range(0,max_powers):
+    print("power of %d is %s"%(1<<i,powers[i]))
   result_so_far = "1"
-  for i in range(max_powers,0,-1):
-    ipow2 = 1<<(i-1)
-    if n > ipow2:
-      print("%s is more than %d, so adding this power %d"%(n,ipow2,i))
+  for i in range(max_powers-1,-1,-1):
+    ipow2 = 1<<(i)
+    if n >= ipow2:
+      print("%s is more or equal than %d, so multiplying this power %d"%(n,ipow2,i))
       result_so_far = multiply_big_number(result_so_far,len(result_so_far),powers[i],len(powers[i]))
       while result_so_far[0] == "0":
         result_so_far = result_so_far[1:]
       n -= ipow2
-  print (result_so_far)
+  if n != 0:
+    print("You should increase the power")
+  print ("%d raised to %s is %s"%(b,sys.argv[2],result_so_far))
 
